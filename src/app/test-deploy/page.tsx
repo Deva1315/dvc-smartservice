2ece7a6b952b4defb647bc1784f9260cb9e537aa
Deva1315 <kidzxgh44@gmail.com>
2026-06-01 19:12:21 +0800

    testing

---
+++ src/app/test-deploy/page.tsx
@@ -0,0 +1,8 @@
+export default function TestDeployPage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Test Deploy OK</h1>
+      <p>Route ini terbaca oleh Vercel.</p>
+    </main>
+  );
+}
